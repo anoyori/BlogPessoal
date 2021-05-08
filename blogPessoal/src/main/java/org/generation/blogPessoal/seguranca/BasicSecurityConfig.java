a8494cba6a7b54e08760550389f41f5ab3bbb777
Amanda <anoyori@gmail.com>
2021-05-07 23:47:21 -0300

    Incluindo camada Usuario Controller

--- blogPessoal/src/main/java/org/generation/blogPessoal/seguranca/BasicSecurityConfig.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/seguranca/BasicSecurityConfig.java
@@ -19,7 +19,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-	auth.userDetailsService(userDetailsService);
+		auth.userDetailsService(userDetailsService);
 	}
 	
 	@Bean
@@ -30,7 +30,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure (HttpSecurity http) throws Exception{
 		http.authorizeRequests()
-		.antMatchers(" ").permitAll()
+		//.antMatchers("").permitAll()
 		.antMatchers("/usuarios/logar").permitAll()
 		.antMatchers("/usuarios/cadastrar").permitAll()
 		.anyRequest().authenticated()
--- blogPessoal/src/main/java/org/generation/blogPessoal/seguranca/UserDetailsImpl.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/seguranca/UserDetailsImpl.java
@@ -1,10 +1,12 @@
 package org.generation.blogPessoal.seguranca;
 
 import java.util.Collection;
+import java.util.List;
 
-import org.generation.blogPessoal.model.Usuario;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import org.generation.blogPessoal.model.Usuario;
 
 public class UserDetailsImpl implements UserDetails{
 	
@@ -12,6 +14,7 @@ public class UserDetailsImpl implements UserDetails{
 	
 	private String userName;
 	private String password;
+	private List<GrantedAuthority> authorities;
 	
 	public UserDetailsImpl(Usuario user) {
 		this.userName = user.getUsuario();
@@ -22,10 +25,9 @@ public class UserDetailsImpl implements UserDetails{
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO Auto-generated method stub
-		return null;
+		return authorities;
 	}
-
+	
 	@Override
 	public String getPassword() {
 		// TODO Auto-generated method stub
